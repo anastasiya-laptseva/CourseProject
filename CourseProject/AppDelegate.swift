7e6955d466a20bb4aa3b17cd264e47479f56948a
Anastasiya Lapteva <anas.alex.novikova@gmail.com>
2022-06-27 22:05:54 +0300

    add auth google

--- CourseProject/AppDelegate.swift
+++ CourseProject/AppDelegate.swift
@@ -7,15 +7,22 @@
 
 import UIKit
 import CoreData
+import FirebaseCore
+import GoogleSignIn
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        return GIDSignIn.sharedInstance.handle(url)
     }
 
     // MARK: UISceneSession Lifecycle
